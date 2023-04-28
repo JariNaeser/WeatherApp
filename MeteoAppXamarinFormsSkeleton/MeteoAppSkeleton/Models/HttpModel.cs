229f5f71d65e3f810a98fe8c388aa8801f7d9ba0
Jari Näser <jari.naeser@supsi.ch>
2023-04-28 15:56:16 +0200

    Added httpManager class for http requests

---
+++ MeteoAppXamarinFormsSkeleton/MeteoAppSkeleton/Models/HttpModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace MeteoAppSkeleton.Models
+{
+	public class HttpModel
+	{
+		private static HttpModel myself;
+
+		public HttpModel getInstance()
+		{
+			if(myself == null)
+			{
+				myself = new HttpModel();
+			}
+			return myself;
+		}
+
+		private HttpModel() { }
+
+		public async Task<WeatherCondition> GetAsync(String url)
+		{
+			// Make request
+			var httpClient = new HttpClient();
+			var jsonResponse = await httpClient.GetStringAsync(url);
+
+			// Parse response
+			return JsonConvert.DeserializeObject<WeatherCondition>(jsonResponse);
+        }
+	}
+}
+
---
+++ MeteoAppXamarinFormsSkeleton/MeteoAppSkeleton/Models/WeatherCondition.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace MeteoAppSkeleton.Models
+{
+	public class WeatherCondition
+	{
+        private int Id { get; set; }
+        private String WeatherType { get; set; }
+        private String Description { get; set; }
+        private String Icon { get; set; }
+        private double Temperature { get; set; }
+        private int Pressure { get; set; }
+        private int Humidity { get; set; }
+        private double MinTemperature { get; set; }
+        private double MaxTemperature { get; set; }
+        private String CityName { get; set; }
+        private String CountryCode { get; set; }
+        private long Sunrise { get; set; }
+        private long Sunset { get; set; }
+        private double WindSpeed { get; set; }
+
+        public WeatherCondition(int id, String weatherType, String description,
+                             String icon, double temperature, int pressure,
+                             int humidity, double minTemperature, double maxTemperature,
+                             String cityName, String countryCode, long sunrise,
+                             long sunset, double windSpeed)
+		{
+            Id = id;
+            WeatherType = weatherType;
+            Description = description;
+            Icon = icon;
+            Temperature = temperature;
+            Pressure = pressure;
+            Humidity = humidity;
+            MinTemperature = minTemperature;
+            MaxTemperature = maxTemperature;
+            CityName = cityName;
+            CountryCode = countryCode;
+            Sunrise = sunrise;
+            Sunset = sunset;
+            WindSpeed = windSpeed;
+		}
+	}
+}
+
